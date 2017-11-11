4d122ec4d139be0ac935d7c63744310f0eec5896
Kévin Duguay <Xbox99972@hotmail.com>
2017-11-10 21:46:29 -0500

    Initialised project

---
+++ Assets/Scripts/Character/PlayerControls.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+// PlayerController script requires thoses components and will be added if they aren't already there
+//[RequireComponent(typeof(CharacterMovement))]
+
+public class PlayerControls : MonoBehaviour
+{
+    //protected CharacterMovement movementScript;
+    
+    [SerializeField]
+    private string usedJoystickName = "";
+
+    public string UsedJoystickName
+    {
+        get { return usedJoystickName; }
+        set { usedJoystickName = value; }
+    }
+
+    private void Awake()
+    {
+        //movementScript = GetComponent<CharacterMovement>();
+    }
+
+    private void Update()
+    {
+        if (Time.deltaTime > 0)
+        {
+            Hashtable inputs = fetchInputs();
+
+            UpdateMovement(inputs);
+
+            //TestInputs(inputs);
+        }
+    }
+
+    // The Hashtable of inputs value must contain those keys:
+    //-verticalInput
+    //-horizontalInput
+    //-xAxis
+    //-yAxis
+    //-submitInput
+    //-startInput
+    //-cancelInput
+    private Hashtable fetchInputs()
+    {
+        Hashtable inputs = new Hashtable();
+
+        //Get the inputs used for the movement
+        inputs.Add("verticalInput", Input.GetAxisRaw(UsedJoystickName + " Vertical"));
+        inputs.Add("horizontalInput", Input.GetAxisRaw(UsedJoystickName + " Horizontal"));
+        inputs.Add("xAxis", Input.GetAxis(UsedJoystickName + " Right Stick X Axis"));
+        inputs.Add("yAxis", Input.GetAxis(UsedJoystickName + " Right Stick Y Axis"));
+        inputs.Add("submitInput", Input.GetButtonDown(UsedJoystickName + " Submit"));
+        inputs.Add("startInput", Input.GetButtonDown(UsedJoystickName + " Start"));
+        inputs.Add("cancelInput", Input.GetButtonDown(UsedJoystickName + " Cancel"));
+        
+        return inputs;
+    }
+
+    private void UpdateMovement(Hashtable inputs)
+    {
+        //movementScript.UpdateMovement(inputs);
+    }
+
+    private void TestInputs(Hashtable inputs)
+    {
+        Debug.Log("verticalInput: " + (float)inputs["verticalInput"]);
+        Debug.Log("horizontalInput: " + (float)inputs["horizontalInput"]);
+        Debug.Log("xAxis: " + (float)inputs["xAxis"]);
+        Debug.Log("yAxis: " + (float)inputs["yAxis"]);
+        Debug.Log("submitInput: " + (bool)inputs["submitInput"]);
+        Debug.Log("startInput: " + (bool)inputs["startInput"]);
+        Debug.Log("cancelInput: " + (bool)inputs["cancelInput"]);
+    }
+}

6685c7c71c31833f3567dbb7ded1414334bfcf69
PereKastor <constant.95@gmail.com>
2017-11-11 15:53:20 -0500

    Add zoom out on launch

--- Assets/Scripts/Game/InitialLaunch.cs
+++ Assets/Scripts/Game/InitialLaunch.cs
@@ -9,9 +9,18 @@ public class InitialLaunch : MonoBehaviour
     public float m_timeToExplode;
     public Vector3 m_positionToExplode;
     public List<GameObject> m_players = new List<GameObject>();
+    public AnimationCurve m_cameraZoomOnLaunch;
+    public AnimationCurve m_fireworkSpeedOnLaunch;
 
     private Rigidbody m_rigidbody;
+    private Camera m_currentCamera;
 
+    // Camera
+    public float m_startFOV;
+    public float m_endFOV;
+    Vector3 m_camStartPosition;
+
+    // Firework
     Vector3 m_startPosition;
     float m_currentLerpTime;
     float m_startLerpTime;
@@ -19,11 +28,13 @@ public class InitialLaunch : MonoBehaviour
     private void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_currentCamera = GetComponentInChildren<Camera>();
     }
     // Use this for initialization
     void Start()
     {
         m_startPosition = transform.position;
+        m_camStartPosition = m_currentCamera.transform.position;
         m_currentLerpTime = 0.0f;
         m_startLerpTime = Time.time;
     }
@@ -32,9 +43,11 @@ public class InitialLaunch : MonoBehaviour
     void Update()
     {
         m_currentLerpTime = Mathf.Clamp01((Time.time - m_startLerpTime) / m_timeToExplode);
-        transform.position = Vector3.Lerp(m_startPosition, m_positionToExplode, m_currentLerpTime);
 
-        if(IsPositionsNear(gameObject.transform.position, m_positionToExplode, 0.01f))
+        transform.position = Vector3.Lerp(m_startPosition, m_positionToExplode, m_fireworkSpeedOnLaunch.Evaluate(m_currentLerpTime));
+        m_currentCamera.fieldOfView = Mathf.Lerp(m_startFOV, m_endFOV, m_cameraZoomOnLaunch.Evaluate(m_currentLerpTime));
+        
+        if (IsPositionsNear(gameObject.transform.position, m_positionToExplode, 0.01f))
         {
             StartGame();
         }
@@ -67,5 +80,11 @@ public class InitialLaunch : MonoBehaviour
     private bool IsPositionsNear(Vector3 p1, Vector3 p2, float threshold)
     {
         return Vector3.Distance(p1, p2) <= threshold;
+    }
+
+
+    public Vector3 Sinerp(Vector3 start, Vector3 end, float value)
+    {
+        return new Vector3(Mathf.Lerp(start.x, end.x, Mathf.Sin(value * Mathf.PI * 0.5f)), Mathf.Lerp(start.y, end.y, Mathf.Sin(value * Mathf.PI * 0.5f)), Mathf.Lerp(start.z, end.z, Mathf.Sin(value * Mathf.PI * 0.5f)));
     }
 }

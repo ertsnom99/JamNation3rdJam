82491efdbc4bedd8100740e77fdbca41ef58ca3c
PereKastor <constant.95@gmail.com>
2017-11-10 23:42:30 -0500

    Dont forget the files...

---
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 m_initialDirection;
+    public float m_currentSpeed;
+    public float m_thresholdUpdate;
+
+    private Rigidbody m_rigidbody;
+    private Vector3 m_currentDirection;
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        m_rigidbody.velocity = m_currentDirection * m_currentSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {}
+
+    public void UpdateMovement(Hashtable inputs)
+    {
+        if (IsOverThreshold((float)inputs["horizontalInput"]) || IsOverThreshold((float)inputs["verticalInput"]))
+        {
+            m_currentDirection.Set((float)inputs["horizontalInput"], (float)inputs["verticalInput"], 0.0f);
+            m_currentDirection.Normalize();
+
+            m_rigidbody.velocity = m_currentDirection * m_currentSpeed;
+        }
+    }
+
+    private bool IsOverThreshold(float value)
+    {
+        return Mathf.Abs(value) >= m_thresholdUpdate;
+    }
+
+    /***
+     * Debug
+     **/
+
+    /// <summary>
+    /// Display a debug message with the given vector
+    /// </summary>
+    /// <param name="vec"></param>
+    private void DebugVector(Vector3 vec)
+    {
+        Debug.Log(string.Format("{0}, {1}, {2}", vec.x, vec.y, vec.z));
+    }
+    /// <summary>
+    /// Display a debug message with the given vector
+    /// </summary>
+    /// <param name="vec"></param>
+    private void DebugVector(Vector2 vec)
+    {
+        Debug.Log(string.Format("{0}, {1}", vec.x, vec.y));
+    }
+}
